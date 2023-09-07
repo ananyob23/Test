ec077428e20c3a602dc0e3eab7132583640823e0
ananyob23 <137041727+ananyob23@users.noreply.github.com>
2023-09-07 11:24:41 +0530

    Update Test_Con_SQL_Server.py

--- Test_Con_SQL_Server.py
+++ Test_Con_SQL_Server.py
@@ -4,7 +4,7 @@
 import streamlit as st
 from streamlit.connections import SQLConnection
 
-conn = st.experimental_connection('Test',type=SQLconnection)
+conn = st.experimental_connection('Test',type='sql')
 mytable = conn.query('select * from mytable')
 st.dataframe(mytable)
 
