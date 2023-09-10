0c9b8f4745eb4282dbf2a12a4cd9dd5607fa8cae
ananyob23 <137041727+ananyob23@users.noreply.github.com>
2023-09-10 11:18:53 +0530

    Update Test_Con_SQL_Server.py

--- Test_Con_SQL_Server.py
+++ Test_Con_SQL_Server.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pyodbc
 # Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
+
 def init_connection():
     return pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
@@ -18,7 +17,7 @@ def init_connection():
 conn = init_connection()
 
 # Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
+
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
